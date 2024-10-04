4ac92a3f15916a52ef205b636c873a1a80cab8ae
mahmudhassanfiroz <mahmudhassanfiroz90@gmail.com>
2024-10-05 00:16:45 +0600

    error solve

--- online_job_exam/settings.py
+++ online_job_exam/settings.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(h80ed#^=df1pc(jdlsbzeo!$kjqk%*xkv2yyyva0)eyhra6-='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '7406-45-124-171-177.ngrok-free.app', '.vercel.app']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(",")
 
 # Application definition
 INSTALLED_APPS = [
@@ -250,3 +251,10 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
 }
+
+#
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'))
+
+
